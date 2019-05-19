63d9bfa3ab83adc081d5237b657ff10987edaaaa
Stephane Van Pe <stephanevanpe@gmail.com>
2019-05-19 12:32:36 +0200

    resolving problems of form

--- src/components/Main.js
+++ src/components/Main.js
@@ -38,7 +38,7 @@ class Main extends React.Component {
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Contact</h2>
-          <form method="post" action="#">
+          <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
