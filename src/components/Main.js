a93c9817ee4aac0d2cd2125075e5f9919d914667
Stephane Van Pe <stephanevanpe@gmail.com>
2019-05-19 22:19:34 +0200

    starting development of a grid of picture

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,80 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Gallery from '../components/Gallery'
 
 import margouillat from '../images/margouillat.jpg'
 import code from '../images/IMG_4193.jpg'
 import pouring from '../images/Pouring.jpg'
 import cv from '../pages/cv Van_Pe_Stephane.pdf'
 
+import thumb01 from '../assets/images/thumbs/01.jpg'
+import thumb02 from '../assets/images/thumbs/02.jpg'
+import thumb03 from '../assets/images/thumbs/03.jpg'
+import thumb04 from '../assets/images/thumbs/04.jpg'
+import thumb05 from '../assets/images/thumbs/05.jpg'
+import thumb06 from '../assets/images/thumbs/06.jpg'
+
+import full01 from '../assets/images/fulls/01.jpg'
+import full02 from '../assets/images/fulls/02.jpg'
+import full03 from '../assets/images/fulls/03.jpg'
+import full04 from '../assets/images/fulls/04.jpg'
+import full05 from '../assets/images/fulls/05.jpg'
+import full06 from '../assets/images/fulls/06.jpg'
+
+const DEFAULT_IMAGES = [
+  { id: '1', src: full01, thumbnail: thumb01, caption: 'Photo 1', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.' },
+  { id: '2', src: full02, thumbnail: thumb02, caption: 'Photo 2', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.' },
+  { id: '3', src: full03, thumbnail: thumb03, caption: 'Photo 3', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.' },
+  { id: '4', src: full04, thumbnail: thumb04, caption: 'Photo 4', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.' },
+  { id: '5', src: full05, thumbnail: thumb05, caption: 'Photo 5', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.' },
+  { id: '6', src: full06, thumbnail: thumb06, caption: 'Photo 6', description: 'Lorem ipsum dolor sit amet nisl sed nullam feugiat.' }
+];
+
 class Main extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      lightboxIsOpen: false,
+      currentImage: 0,
+    };
+
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+    this.handleClickImage = this.handleClickImage.bind(this);
+  }
+
+  openLightbox(index, event) {
+    event.preventDefault();
+    this.setState({
+      currentImage: index,
+      lightboxIsOpen: true,
+    });
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
+  handleClickImage() {
+    if (this.state.currentImage === this.props.images.length - 1) return;
+
+    this.gotoNext();
+  }
+
   render() {
 
     let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
@@ -33,8 +101,17 @@ class Main extends React.Component {
         <article id="about" className={`${this.props.article === 'about' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">A Propos</h2>
           <span className="image main"><img src={pouring} alt="" /></span>
+          <div>
           <p>Il est très dure de ce décrire sois même. Passionné par un tas de choses je vous laisse le soin de me découvrir via les photos suivantes. </p>
-          <p>J'espère que cela vous a plus, n'hesitez pas à me contacter.</p>          {close}
+          <Gallery images={DEFAULT_IMAGES.map(({ id, src, thumbnail, caption, description }) => ({
+            src,
+            thumbnail,
+            caption,
+            description
+          }))} />
+          <p>J'espère que cela vous a plus, n'hesitez pas à me contacter.</p>
+          </div>
+          {close}
         </article>
 
         <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
