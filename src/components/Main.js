602a3790685656ba24e1afd11bca7be9a7851be4
Stephane Van Pe <stephanevanpe@gmail.com>
2020-05-14 10:54:07 +0200

    changing text in intro

--- src/components/Main.js
+++ src/components/Main.js
@@ -56,67 +56,104 @@ class Main extends React.Component {
     let close = <div className="close" onClick={() => {this.props.onCloseArticle()}}></div>
 
     return (
-      <div ref={this.props.setWrapperRef} id="main" style={this.props.timeout ? {display: 'flex'} : {display: 'none'}}>
+					<div ref={this.props.setWrapperRef} id='main' style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}>
+						<article
+							id='intro'
+							className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`}
+							style={{ display: 'none' }}
+						>
+							<h2 className='major'>Intro</h2>
+							<span className='image main'>
+								<img src={margouillat} alt='' />
+							</span>
+							<p>Bienvenue sur mon site personnel.</p>
+							<p>Après plusieurs années de travail dans le secteur de la biologie marine, j'ai du faire face à un changement de vie.</p>
+							<p>
+								C'est pour cela que début 2019, j'ai debuté ma formation de developpeur web au sein de la Wild Code School de Bidart. Durant mon cursus de
+								formation, j'ai pu étudier: <b>Html/CSS, JavaScript, React, SQL</b>. De même plusieurs projets ont été réalisés; vous pouvez les retrouver sur
+								mon github.
+							</p>
+							<p>Ce site a été créé afin que vous puissiez découvrir mon travail de jeune developpeur .</p>
+							<p>Bonne visite . </p>
+							{close}
+						</article>
 
-        <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
-          <h2 className="major">Intro</h2>
-          <span className="image main"><img src={margouillat} alt="" /></span>
-          <p>Bienvenue sur mon site personnel.</p>
-          <p>Après plusieurs années de travail dans le secteur de la biologie marine, j'ai du faire face à un changement de vie.</p>
-          <p>C'est pour cela que début 2019, j'ai debuté ma formation de developpeur web au sein de la Wild Code School de Bidart. Durant mon cursus de formation, j'ai pu étudier: Html/CSS, JavaScript, React, SQL. De même plusieurs projets ont été réalisés; vous pouvez les retrouver sur mon github.</p>
-          <p>Ce site a été créé afin que vous puissiez découvrir mon travail de jeune developpeur .</p>
-          <p>Bonne visite . </p>
-          {close}
-        </article>
+						<article
+							id='work'
+							className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`}
+							style={{ display: 'none' }}
+						>
+							<h2 className='major'>Réalisations</h2>
+							<span className='image main'>
+								<img src={code} alt='' />
+							</span>
+							<p>
+								Projet réalisé en stage et/ou emploi :
+								<li> Le lien de la web app I Clean My Sea serra présent lorsque celui-ci aurra été dévoiller au public.</li>
+								<li>
+									<a target='_blank' href='https://khrisma.fr'>
+										Khrisma
+									</a>
+								</li>
+							</p>
+							<p>
+								Projet réalisé en formation :
+								<li>
+									<a target='_blank' href='https://blogteam.netlify.app/'>
+										Blog Big Pig Junk Food
+									</a>
+								</li>
+							</p>
 
-        <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{ display: 'none' }}>
-          <h2 className="major">Réalisations</h2>
-          <span className="image main"><img src={code} alt="" /></span>
-          <p>Projet réalisé en stage et/ou emploi :
-            <li> Le lien de la web app I Clean My Sea serra présent lorsque celui-ci aurra été dévoiller au public.</li>
-            <li><a target="_blank" href="https://khrisma.fr">Khrisma</a></li>
-          </p>
-          <p>Projet réalisé en formation :
-            <li><a target="_blank" href="https://blogteam.netlify.app/">Blog Big Pig Junk Food</a></li>
-          </p>
-          
-          
-          {close}
-        </article>
+							{close}
+						</article>
 
-
-        <article id="contact" className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
-          <h2 className="major">Contact</h2>
-          <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-            <input type="hidden" name="bot-field" />
-            <input type="hidden" name="form-name" value="contact" />
-            <div className="field half first">
-              <label htmlFor="name">Name</label>
-              <input type="text" name="name" id="name" />
-            </div>
-            <div className="field half">
-              <label htmlFor="email">Email</label>
-              <input type="text" name="email" id="email" />
-            </div>
-            <div className="field">
-              <label htmlFor="message">Message</label>
-              <textarea name="message" id="message" rows="4"></textarea>
-            </div>
-            <ul className="actions">
-              <li><input type="submit" value="Send Message" className="special" /></li>
-              <li><input type="reset" value="Reset" /></li>
-            </ul>
-          </form>
-          <ul className="icons">
-            <li><a href="https://github.com/stephanevanpe" className="icon fa-github"><span className="label">GitHub</span></a></li>
-            <li><a href="https://fr.linkedin.com/in/stephane-van-pe" className="icon fa-linkedin"><span className="label">Linkedin</span></a></li>
-
-          </ul>
-          {close}
-        </article>
-
-      </div>
-    )
+						<article
+							id='contact'
+							className={`${this.props.article === 'contact' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`}
+							style={{ display: 'none' }}
+						>
+							<h2 className='major'>Contact</h2>
+							<form name='contact-form' method='post' data-netlify='true' data-netlify-honeypot='bot-field'>
+								<input type='hidden' name='bot-field' />
+								<input type='hidden' name='form-name' value='contact' />
+								<div className='field half first'>
+									<label htmlFor='name'>Name</label>
+									<input type='text' name='name' id='name' />
+								</div>
+								<div className='field half'>
+									<label htmlFor='email'>Email</label>
+									<input type='text' name='email' id='email' />
+								</div>
+								<div className='field'>
+									<label htmlFor='message'>Message</label>
+									<textarea name='message' id='message' rows='4'></textarea>
+								</div>
+								<ul className='actions'>
+									<li>
+										<input type='submit' value='Send Message' className='special' />
+									</li>
+									<li>
+										<input type='reset' value='Reset' />
+									</li>
+								</ul>
+							</form>
+							<ul className='icons'>
+								<li>
+									<a href='https://github.com/stephanevanpe' className='icon fa-github'>
+										<span className='label'>GitHub</span>
+									</a>
+								</li>
+								<li>
+									<a href='https://fr.linkedin.com/in/stephane-van-pe' className='icon fa-linkedin'>
+										<span className='label'>Linkedin</span>
+									</a>
+								</li>
+							</ul>
+							{close}
+						</article>
+					</div>
+				);
   }
 }
 
