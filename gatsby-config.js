b9d5ccb7347bed465590db18fb4e8884dd05d238
Stephane Van Pe <stephanevanpe@gmail.com>
2020-05-15 10:03:27 +0200

    adding a new plugin for analytics because nothing run correctly and i have modifie the file gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,39 @@
 module.exports = {
-  siteMetadata: {
-    title: "Mon site personnel",
-    author: "Stephane Van Pe",
-    description: "Site personnel de présentation",
-    siteUrl: `https://stephane-van-pe.netlify.com`
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: 'UA-104843706-4',
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
- // This path is relative to the root of the site.
-      },
-    },
-    'gatsby-plugin-sass',
-    'gatsby-plugin-offline',
-    `gatsby-plugin-sitemap`    
-  ],
-}
+	siteMetadata: {
+		title: 'Mon site personnel',
+		author: 'Stephane Van Pe',
+		description: 'Site personnel de présentation',
+		siteUrl: `https://stephane-van-pe.netlify.com`,
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				trackingId: 'UA-104843706-4',
+			},
+		},
+		{
+			resolve: `gatsby-plugin-google-analytics`,
+			options: {
+				// replace "UA-XXXXXXXXX-X" with your own Tracking ID
+				trackingId: 'G-SNN3ES0MS3',
+			},
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: 'gatsby-starter-default',
+				short_name: 'starter',
+				start_url: '/',
+				background_color: '#663399',
+				theme_color: '#663399',
+				display: 'minimal-ui',
+				// This path is relative to the root of the site.
+			},
+		},
+		'gatsby-plugin-sass',
+		'gatsby-plugin-offline',
+		`gatsby-plugin-sitemap`,
+	],
+};
