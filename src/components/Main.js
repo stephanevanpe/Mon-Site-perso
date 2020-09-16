66686b004d1bcf7de74a95122359389e4097f49b
Stephane Van Pe <stephanevanpe@gmail.com>
2020-09-16 22:14:57 +0200

    adding the personal project of the todo list

--- src/components/Main.js
+++ src/components/Main.js
@@ -120,6 +120,11 @@ class Main extends React.Component {
 									<u>Projets personnels :</u>
 								</h3>
 								<li>
+									<a target='_blank' href='https://todo-listsvp.netlify.app/'>
+										<b>Todo list</b>
+									</a>
+								</li>
+								<li>
 									<a target='_blank' href='https://morpionsvp.netlify.app'>
 										<b>Jeu du morpion</b>
 									</a>
